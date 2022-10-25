72b8fe177665cdf2ec7247c82dc595c02a1109c7
Willian Sant Anna <w.s.santanna@gmail.com>
2022-10-26 03:18:55 +0400

    Implementacao do provider do carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom"
+import { CartProvider } from "./providers/cart.provider"
 
 function App() {
 
-  return (
-    <>
-      <h1>Logo</h1>
-      <Outlet />
-    </>
-  )
+	return (
+		<>
+			<h1>Logo</h1>
+
+			<CartProvider>
+				<Outlet />
+			</CartProvider>
+		</>
+	)
 }
 
 export default App
