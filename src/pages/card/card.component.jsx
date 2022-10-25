05545ac0e8ed4b486bf0046a5ef32bdb8290ae72
Willian Sant Anna <w.s.santanna@gmail.com>
2022-10-26 03:35:32 +0400

    Implementacao do servico de comunicacao dos produtos

---
+++ src/pages/card/card.component.jsx
@@ -0,0 +1,10 @@
+
+export default function Card() {
+
+
+    return (
+        <>
+            <h1>Meu carrinho</h1>
+        </>
+    )
+}
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,7 +1,44 @@
 
 const ProductService = {
 
+    GetMany: async function () {
 
+        try {
+            const request = await fetch('https://fakestoreapi.com/products');
+            const products = await request.json();
+
+            return products;
+        }
+        catch {
+            return { message: 'Nao foi possivel finalizar a operacao' }
+        }
+    },
+
+    GetById: async function (idProduct) {
+
+        try {
+            const request = await fetch(`https://fakestoreapi.com/products/${idProduct}`);
+            const currentProduct = await request.json();
+
+            return currentProduct;
+        }
+        catch {
+            return { message: 'Nao foi possivel finalizar a operacao' }
+        }
+
+    },
+
+    GetByCategory: async function (categoryName) {
+        try {
+            const request = await fetch(`https://fakestoreapi.com/products/category/${categoryName}`);
+            const productsFound = await request.json();
+
+            return productsFound;
+        }
+        catch {
+            return { message: 'Nao foi possivel finalizar a operacao' }
+        }
+    }
 
 }
 
